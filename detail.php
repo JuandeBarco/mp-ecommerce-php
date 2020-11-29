4f95dafa88519e7a13218545cb80cc1b75362b63
JuandeBarco <lextadz@gmail.com>
2020-11-29 14:03:45 -0600

    Update detail.php

--- detail.php
+++ detail.php
@@ -3,7 +3,7 @@
     require __DIR__ .  '/vendor/autoload.php';
 
     // Agrega credenciales
-    MercadoPago\SDK::setAccessToken(' APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
+    MercadoPago\SDK::setAccessToken('APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
 
     // Crea un objeto de preferencia
     $preference = new MercadoPago\Preference();
