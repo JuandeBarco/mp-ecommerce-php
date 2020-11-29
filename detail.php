50d9f23c6d947dbee72dc22e32059f3c530f0cda
JuandeBarco <lextadz@gmail.com>
2020-11-29 13:56:03 -0600

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,6 +1,6 @@
 <?php
     // SDK de Mercado Pago
-    require __DIR__ .  './vendor/autoload.php';
+    require __DIR__ .  '/vendor/autoload.php';
 
     // Agrega credenciales
     MercadoPago\SDK::setAccessToken(' APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181');
