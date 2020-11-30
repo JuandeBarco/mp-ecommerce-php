3ab6a16ad659881437cfe3a2eb3b4a0b83db08fa
JuandeBarco <lextadz@gmail.com>
2020-11-29 22:36:44 -0600

    v1.3

--- detail.php
+++ detail.php
@@ -14,7 +14,7 @@
     $item->id = "1234";
     $item->title = $_POST['title'];
     $item->description = "Dispositivo móvil de Tienda e-commerce";
-    $item->picture_url = $_POST['img'];
+    $item->picture_url = "https://juandebarco-mp-commerce-php.herokuapp.com/" . $_POST['img'];
     $item->category_id = "phones";
     $item->quantity = $_POST['unit'];
     $item->currency_id = "MXN";
@@ -152,7 +152,7 @@
                                             <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
                                                 <div class="as-tilegallery-element as-image-selected">
                                                     <div class=""></div>
-                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo $_POST['img'] ?>) 2x);">
+                                                    <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(<?php echo './' . $_POST['img'] ?>) 2x);">
                                                 </div>
                                                 
                                             </div>
