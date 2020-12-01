eaa75c0ca2b4e988b7061f69ac94501ced304646
JuandeBarco <lextadz@gmail.com>
2020-11-30 23:04:46 -0600

    Update endpoint.php

--- endpoint.php
+++ endpoint.php
@@ -1,41 +1,33 @@
 <?php
     $servername = "rcdistribuidora.com";
+    $database = "rcdistri_mpexample";
     $username = "rcdistri_mp";
     $password = "qwerty123";
-    $dbname = "rcdistri_mpexample";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
     }
     
+    echo "Connected successfully";
+
     // SDK de Mercado Pago
     require __DIR__ .  '/vendor/autoload.php';
     
     // Agrega notificación
     MercadoPago\SDK::setAccessToken("APP_USR-1159009372558727-072921-8d0b9980c7494985a5abd19fbe921a3d-617633181");
     $info = json_decode(file_get_contents("php://input"));
-    $sql = "INSERT INTO json_table (json_texto) VALUES ($info)";
 
-    echo $info;
+    $texto = json_encode($info);
     
-    /*switch($_POST["type"]) {
-        case "payment":
-            $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
-            $json_texto = json_decode(MercadoPago\Payment);
-            break;
-        case "plan":
-            $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
-            break;
-            case "subscription":
-                $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
-            break;
-            case "invoice":
-                $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
-            break;
-        }*/
-    $conn->query($sql);
-    $conn->close();
+    $sql = "INSERT INTO json_table (json_j) VALUES ('$texto')";
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+    mysqli_close($conn);
 ?>
